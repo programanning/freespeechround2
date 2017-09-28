b07a5a6cc38eece6763085f3b44980dc3f8b6fdf
programanning <emanning@nevada.unr.edu>
2017-09-27 22:14:37 -0700

    init commit

---
+++ app/src/main/java/com/example/erikamanning/loaf/LockScreen.java
@@ -0,0 +1,15 @@
+package com.example.erikamanning.loaf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LockScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+}
+
+
